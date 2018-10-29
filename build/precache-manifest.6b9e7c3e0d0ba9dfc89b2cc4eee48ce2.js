9d4142cba7776d6319cf2bec00d2f236d9ed48b1
Ashik Achaparambil <getashik@gmail.com>
2018-10-29 15:38:43 -0500

    first push

---
+++ build/precache-manifest.6b9e7c3e0d0ba9dfc89b2cc4eee48ce2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "326b1047e6c0c95266e7",
+    "url": "/static/js/main.326b1047.chunk.js"
+  },
+  {
+    "revision": "ad721204161f56fc5a30",
+    "url": "/static/js/1.ad721204.chunk.js"
+  },
+  {
+    "revision": "326b1047e6c0c95266e7",
+    "url": "/static/css/main.6d5df531.chunk.css"
+  },
+  {
+    "revision": "e508a11e721dce278fc73e2ed18c88f6",
+    "url": "/index.html"
+  }
+];
---
+++ build/precache-manifest.a789b8cf2474d7e539fc854ea834c453.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c92242924c053cb84727",
+    "url": "/static/js/main.c9224292.chunk.js"
+  },
+  {
+    "revision": "ad721204161f56fc5a30",
+    "url": "/static/js/1.ad721204.chunk.js"
+  },
+  {
+    "revision": "c92242924c053cb84727",
+    "url": "/static/css/main.6d5df531.chunk.css"
+  },
+  {
+    "revision": "2b44ad0360bba18c8cbdce23baf8a3ac",
+    "url": "/index.html"
+  }
+];
